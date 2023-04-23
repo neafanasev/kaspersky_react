35208b2fb35a2512f60088e1eea6273a2fd2c313
neafanasev <neafanasev@gmail.com>
2023-04-23 23:22:42 +0300

    changed welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -15,49 +15,49 @@ const Welcome = () => (
           py={4}
     >
 
-      <Text ml={0} as={'b'} fontSize={'xl'}>{'const aboutProject = {'}</Text>
+      <Text ml={0} as={'b'} fontSize={'md'}>{'const aboutProject = {'}</Text>
 
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'stack: {'}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'frontend: [\'react\', \'redux\', \'chackra-ui]\','}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'backend: [\'express\']'}</Text>
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'},'}</Text>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'stack: {'}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'frontend: [\'react\', \'redux\', \'chackra-ui]\','}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'backend: [\'express\']'}</Text>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'},'}</Text>
 
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'timeComplexity: {'}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'groupping: \'O(1)\'\','}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'search: \'O(n)\''}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'sort: \'O(n log n)\''}</Text>
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'},'}</Text>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'timeComplexity: {'}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'groupping: \'O(1)\'\','}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'search: \'O(n)\''}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'sort: \'O(n log n)\''}</Text>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'},'}</Text>
 
 
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'contacts: {'}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'fullName: \'Nikita Afanasev\','}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'email: \'niktin242003@gmail.com\','}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>{'phone: \'+79221824676\','}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'contacts: {'}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'fullName: \'Nikita Afanasev\','}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'email: \'niktin242003@gmail.com\','}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>{'phone: \'+79221824676\','}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>
         {'telegram: '}
         <Link href={'https://webk.telegram.org/#@n1ko1337'} isExternal>
           {'\''}@n1ko1337{'\''},
         </Link>
       </Text>
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'},'}</Text>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'},'}</Text>
 
 
-      <Text ml={8} as={'b'} fontSize={'xl'}>{'github: {'}</Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'github: {'}</Text>
+      <Text ml={16} as={'b'} fontSize={'md'}>
         {'frontend: '}
         <Link href={'https://github.com/neafanasev/kaspersky_react'} isExternal>
           {'\''}https://github.com/neafanasev/kaspersky_react{'\''},
         </Link>
       </Text>
-      <Text ml={16} as={'b'} fontSize={'xl'}>
+      <Text ml={16} as={'b'} fontSize={'md'}>
         {'backend: '}
         <Link href={'https://github.com/neafanasev/kaspersky_nodejs'} isExternal>
           {'\''}https://github.com/neafanasev/kaspersky_nodejs{'\''}
         </Link>
       </Text>
 
-      <Text ml={8} as={'b'} fontSize={'2xl'}>{'}'}</Text>
-      <Text ml={0} as={'b'} fontSize={'2xl'}>{'}'}</Text>
+      <Text ml={8} as={'b'} fontSize={'md'}>{'}'}</Text>
+      <Text ml={0} as={'b'} fontSize={'md'}>{'}'}</Text>
 
 
     </Flex>
